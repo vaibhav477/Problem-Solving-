8b95df2dee9628e64105365d09e44456185a3163
vaibhav477 <89143584+vaibhav477@users.noreply.github.com>
2022-08-16 02:12:48 +0530

    Time: 314 ms (63.76%), Space: 22.7 MB (77.13%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-   bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
-        for(int j=0; j<s.size(); j+=wordLen) {
-            string w = s.substr(j, wordLen);
-            if(wordCount.find(w) != wordCount.end()) {
-                if(--wordCount[w] == -1) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> res;
-        int wordLen = words[0].size();
-        int sLen = s.size();
-        int wordsWindow = words.size() * wordLen;
-        
-        unordered_map<string, int> wordCount;
-        for(int i=0; i<words.size(); i++) {
-            wordCount[words[i]]++;
+        int n = s.size(), k = words[0].size(), l = words.size();
+        map<string, int> mp;
+        for(int i = 0; i < l; i++){
+            mp[words[i]]++;
         }
         
-        int i = 0;
-        while(i + wordsWindow <= sLen) {
-            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
-                res.push_back(i);
+        vector<int> ans;
+        
+        for(int i = 0; i < (n+1-(k*l)); i++){
+            map<string, int> help;
+            for(int j = i; j < i+(l*k); j += k){
+                string temp = s.substr(j , k);
+                if(mp.find(temp) != mp.end()){
+                    help[temp]++;
+                }
+                else{
+                    break;
+                }
+                
+                if(help == mp){
+                    ans.push_back(i);
+                }
             }
-            i++;
         }
-        return res;
+        
+        return ans;
     }
 };
