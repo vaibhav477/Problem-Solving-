217bddf18164c3743684674d6ab0ebe81171770c
vaibhav477 <89143584+vaibhav477@users.noreply.github.com>
2022-09-08 04:52:10 +0530

    Time: 1003 ms (5.12%), Space: 124.6 MB (5.92%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,23 +1,59 @@
 class Solution {
 public:
-    // sliding window technique
+    // binary search approach
+    
+    int helper(vector<long long>& pre, int idx, long long value, int k){
+        int i = 0, j = idx, m, ans = -1;
+        while(i <= j){                    // we have to include i=j case also
+            long long sum = pre[idx];
+            m = (i+j)/2;
+            if(m!=0){
+                sum -= pre[m-1];
+            }
+            
+            if((idx-m+1)*(value)*1LL - sum <= k){
+                ans = m;
+                j = m-1;
+            }
+            else{
+                i = m+1;
+            }
+        }
+        return ans;
+    }
+    
     
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        
-        int i = 0, j = 0, maxi = 0;
-        long long sum = 0;
-        while(i < n){
-            sum += nums[i];  // also considering the element in which we have to convert previous elements                                   into.
-            
-            while(j < i && (nums[i]*1LL*(i-j+1) - sum) > k){    // decreasing the window size if total
-                sum -= nums[j++];                              // steps is exceeding k.
-            }                                                 // 1LL is used to convert any value to long                                                                    long.
-            
-            maxi = max(maxi, (i-j+1));   // storing the maximum length
-            i++; // moving window forward
+        map<int,int> mp;
+        for(int i = 0; i < n ;i++){
+            mp[nums[i]]++;
         }
+        
+        vector<long long> pre(n);
+        pre[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            pre[i] = pre[i-1]+nums[i];
+        }
+        
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            if(mp[nums[i]] == 0){
+                continue;
+            }
+            
+            int j = helper(pre, i-1, nums[i], k);
+            if(j != -1){
+                maxi = max(maxi, i-j+mp[nums[i]]);
+            }
+            else{
+                maxi = max(maxi, mp[nums[i]]);
+            }
+            
+            mp[nums[i]] = 0;
+        }
+        
         return maxi;
     }
 };
