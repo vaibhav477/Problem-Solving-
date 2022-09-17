e9aca497426ccc3f4a4d838809768fbe7c049dfc
vaibhav477 <89143584+vaibhav477@users.noreply.github.com>
2022-09-17 09:32:49 +0530

    Time: 989 ms (22.31%), Space: 144.3 MB (39.35%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-         int n = changed.size();
-        if (n % 2 == 1) return {};
+        int n = changed.size();
         sort(changed.begin(), changed.end());
-        vector<int> ans;
-        map<int, int> mp;
-        for (int i = 0; i < n; i++) {
+        
+        map<int,int> mp;
+        for(int i = 0; i < n; i++){
             mp[changed[i]]++;
         }
-        for (int i = 0; i < n; i++) {
-          if (mp[changed[i]] == 0) continue;
-          if (mp[changed[i] * 2] == 0) return {};
-          ans.push_back(changed[i]);
-          mp[changed[i]]--;
-          mp[changed[i] * 2]--;
+        
+        int i;
+        vector<int> ans;
+        for(i = 0; i < n; i++){
+            if(mp[changed[i]] > 0){
+                mp[changed[i]]--;
+                if(mp[2*changed[i]] > 0){
+                    ans.push_back(changed[i]);
+                    mp[2*changed[i]]--;
+                }
+                else{
+                    break;
+                }
+            }
         }
+        
+        if((i != n) || (n%2 != 0)){
+            ans.clear();
+        }
+        
         return ans;
     }
 };
